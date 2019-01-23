f5e1ea65c79c5637f0680ffc5c4705443dd457ea
Chloe <chloemontgomery01@gmail.com>
2019-01-23 13:58:39 +0000

    First params added.

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
 require 'sinatra/base'
+# require 'capybara'
+# require 'capybara/rspec'
+# require 'rspec'
 
 class Battle < Sinatra::Base
   # set :sessions, true
   # set :foo, 'bar'
 
   get '/' do
-    'Hello Battle!'
+    'Testing infrastructure working!'
   end
+
+  get '/start_fight' do
+    erb :start_a_fight
+  end
+
+  post '/fight' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    # erb(:index)
+    p params
+  end
+
+
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,12 +11,22 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+ENV['RACK_ENV'] = 'test'
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = Battle
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
