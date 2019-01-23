6e310f29075aaf6742d0348de17d90f88f832e4d
Sebastian Lavender <sebastianlavender@outlook.com>
2019-01-23 15:34:05 +0000

    add web_helper

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,22 +1,18 @@
 feature 'Testing infrastructure' do
+
   scenario 'Can run app and check page content' do
     visit('/')
     expect(page).to have_content 'Player 1 :'
     expect(page).to have_content 'Player 2 :'
   end
+
   scenario 'Can post player names' do
-    visit('/')
-    fill_in('player1', with: 'name1')
-    fill_in('player2', with: 'name2')
-    click_button('Submit!')
+    sign_in_and_play
     expect(page).to have_content 'name1 vs. name2'
   end
-  scenario 'Displays the player\'s points' do
-    visit('/')
-    fill_in('player1', with: 'name1')
-    fill_in('player2', with: 'name2')
-    click_button('Submit!')
-    expect(page).to have_content 'name2\'s hit points:'
 
+  scenario 'Displays the player\'s points' do
+    sign_in_and_play
+    expect(page).to have_content 'name2\'s hit points:'
   end
 end
