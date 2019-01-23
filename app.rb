7a40a39815e8c663723c29c6065734f62d49aac8
Sebastian Lavender <sebastianlavender@outlook.com>
2019-01-23 14:57:56 +0000

    Used post redirect

--- app.rb
+++ app.rb
@@ -2,18 +2,22 @@ require 'sinatra/base'
 
 
 class Battle < Sinatra::Base
-  # set :sessions, true
+  set :sessions, true
 
   get '/' do
       erb :index
   end
 
-  post '/fight' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
-     erb :play
+  post '/name' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
   end
 
-
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb :play
+  end
 
 end
